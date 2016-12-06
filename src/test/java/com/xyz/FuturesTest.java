845fece206353c68adbb5276ef28fb5553147ac9
qyt <qinyaoting@gmail.com>
2016-12-06 18:41:44 +0800

    Futures

---
+++ src/test/java/com/xyz/FuturesTest.java
@@ -0,0 +1,60 @@
+package com.xyz;
+
+import com.google.common.util.concurrent.*;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by chin on 12/6/16.
+ */
+public class FuturesTest {
+
+
+    @Test
+    public void should_test_furture() throws Exception {
+        ListeningExecutorService service = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
+
+        ListenableFuture future1 = service.submit(new Callable<Integer>() {
+            public Integer call() throws InterruptedException {
+                Thread.sleep(1000);
+                System.out.println("call future 1.");
+                return 1;
+            }
+        });
+
+        ListenableFuture future2 = service.submit(new Callable<Integer>() {
+            public Integer call() throws InterruptedException {
+                Thread.sleep(1000);
+                System.out.println("call future 2.");
+                //       throw new RuntimeException("----call future 2.");
+                return 2;
+            }
+        });
+
+        final ListenableFuture allFutures = Futures.allAsList(future1, future2);
+
+        final ListenableFuture transform = Futures.transform(allFutures, new AsyncFunction<List<Integer>, Boolean>() {
+            @Override
+            public ListenableFuture apply(List<Integer> results) throws Exception {
+                return Futures.immediateFuture(String.format("success future:%d", results.size()));
+            }
+        });
+
+        Futures.addCallback(transform, new FutureCallback<Object>() {
+
+            public void onSuccess(Object result) {
+                System.out.println(result.getClass());
+                System.out.printf("success with: %s%n", result);
+            }
+
+            public void onFailure(Throwable thrown) {
+                System.out.printf("onFailure%s%n", thrown.getMessage());
+            }
+        });
+
+        System.out.println(transform.get());
+    }
+}
